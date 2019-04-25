d19f167b6bb3685199737c87b4d285f65f0761ce
Rainsong <eder.gabriel.felix@gmail.com>
2019-04-24 21:33:12 -0300

    Added dungeon generator

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+extern crate rand;
 extern crate tcod;
 
-use tcod::console::*;
+use std::cmp;
+
 use tcod::colors;
 use tcod::colors::Color;
+use tcod::console::*;
 use tcod::input::Key;
 use tcod::input::KeyCode::*;
+
+use rand::Rng;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
@@ -13,8 +18,21 @@ const FPS_LIMIT: i32 = 120;
 const MAP_WIDTH: i32 = 80;
 const MAP_HEIGHT: i32 = 45;
 
-const COLOR_DARK_WALL: Color = Color {r: 0, g: 0, b: 100};
-const COLOR_DARK_GROUND: Color = Color {r: 50, g: 50, b: 150};
+const ROOM_MAX_SIZE: i32 = 10;
+const ROOM_MIN_SIZE: i32 = 6;
+
+const MAX_ROOMS: i32 = 30;
+
+const MAX_ROOMS_MONSTERS: i32 = 3;
+
+const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_LIGHT_WALL: Color = Color { r: 130, g: 110, b: 50 };
+const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150 };
+const COLOR_LIGHT_GROUND: Color = Color { r: 200, g: 180, b: 50 };
+
+// const FOV_ALGO: FovAlgorithm = FovAlgorithm::Basic;
+// const FOV_LIGHT_WALLS: bool = true;
+// const TORCH_RADIUS: i32 = 10;
 
 type Map = Vec<Vec<Tile>>;
 
@@ -32,8 +50,17 @@ impl Object {
             x: x,
             y: y,
             char: char,
-            color: color
+            color: color,
         }
+    }
+
+    pub fn pos(&self) -> (i32, i32) {
+        (self.x, self.y)
+    }
+
+    pub fn set_pos(&mut self, x: i32, y: i32) {
+        self.x = x;
+        self.y = y;
     }
 
     pub fn move_by(&mut self, dx: i32, dy: i32, map: &Map) {
@@ -61,22 +88,140 @@ struct Tile {
 
 impl Tile {
     pub fn empty() -> Self {
-        Tile{blocked: false, block_sight: false}
+        Tile {
+            blocked: false,
+            block_sight: false,
+        }
     }
 
     pub fn wall() -> Self {
-        Tile{blocked: true, block_sight: true}
+        Tile {
+            blocked: true,
+            block_sight: true,
+        }
     }
 }
 
-fn make_map() -> Map {
-    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
-    map[30][22] = Tile::wall();
-    map[50][22] = Tile::wall();
-    map
+#[derive(Clone, Copy, Debug)]
+struct Rect {
+    x1: i32,
+    x2: i32,
+    y1: i32,
+    y2: i32,
 }
 
-fn render_all(root: &mut Root, con :&mut Offscreen, objects: &[Object], map: &Map ){
+impl Rect {
+    pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
+        Rect {
+            x1: x,
+            y1: y,
+            x2: x + w,
+            y2: y + h,
+        }
+    }
+
+    pub fn center(&self) -> (i32, i32) {
+        let center_x = (self.x1 + self.x2) / 2;
+        let center_y = (self.y1 + self.y2) / 2;
+        (center_x, center_y)
+    }
+
+    pub fn intersects_with(&self, other: &Rect) -> bool {
+        (self.x1 <= other.x2)
+            && (self.x2 >= other.x1)
+            && (self.y1 <= other.y2)
+            && (self.y2 >= other.y1)
+    }
+}
+
+fn create_room(room: Rect, map: &mut Map) {
+    for x in (room.x1 + 1)..room.x2 {
+        for y in (room.y1 + 1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
+}
+
+fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
+    for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+    for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+        map[x as usize][y as usize] = Tile::empty();
+    }
+}
+
+fn make_map(objects: &mut Vec<Object>) -> (Map, (i32, i32)) {
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
+
+    let mut starting_position = (0, 0);
+
+    let mut rooms = vec![];
+
+    for _ in 0..MAX_ROOMS {
+        let w = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+        let h = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+
+        let x = rand::thread_rng().gen_range(0, MAP_WIDTH - w);
+        let y = rand::thread_rng().gen_range(0, MAP_HEIGHT - h);
+
+        let new_room = Rect::new(x, y, w, h);
+
+        let failed = rooms
+            .iter()
+            .any(|other_room| new_room.intersects_with(other_room));
+
+        if failed {
+            continue;
+        }
+
+        create_room(new_room, &mut map);
+        place_objects(new_room, objects);
+
+        let (new_x, new_y) = new_room.center();
+
+        if rooms.is_empty() {
+            starting_position = (new_x, new_y);
+        } else {
+
+            let (prev_x, prev_y) = rooms[rooms.len() - 1 ].center();
+
+            if rand::random() {
+                create_h_tunnel(prev_x, new_x, prev_y, &mut map);
+                create_v_tunnel(prev_y, new_y, new_x, &mut map);
+            } else {
+                create_v_tunnel(prev_y, new_y, prev_x, &mut map);
+                create_h_tunnel(prev_x, new_x, new_y, &mut map);
+            }
+        }
+
+        rooms.push(new_room);
+    }
+
+    (map, starting_position)
+}
+
+fn place_objects(room: Rect, objects: &mut Vec<Object>) {
+    let num_monsters = rand::thread_rng().gen_range(0, MAX_ROOMS_MONSTERS + 1);
+
+    for _ in 0..num_monsters {
+        let x = rand::thread_rng().gen_range(room.x1 + 1, room.x2);
+        let y = rand::thread_rng().gen_range(room.y1 + 1, room.y2);
+
+        let mut monster: Object = if rand::random::<f32>() < 0.8 {
+            Object::new(x, y, 'o', colors::DESATURATED_GREEN)
+        } else {
+            Object::new(x, y, 'T', colors::DARKER_GREEN)
+        };
+
+        objects.push(monster);
+    }
+}
+
+fn render_all(root: &mut Root, con: &mut Offscreen, objects: &[Object], map: &Map) {
     for object in objects {
         object.draw(con);
     }
@@ -85,7 +230,7 @@ fn render_all(root: &mut Root, con :&mut Offscreen, objects: &[Object], map: &Ma
             let wall = map[x as usize][y as usize].block_sight;
             if wall {
                 con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
-            }else{
+            } else {
                 con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
             }
         }
@@ -98,7 +243,11 @@ fn render_all(root: &mut Root, con :&mut Offscreen, objects: &[Object], map: &Ma
 fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
     let key = root.wait_for_keypress(true);
     match key {
-        Key { code: Enter, alt: true, ..} => {
+        Key {
+            code: Enter,
+            alt: true,
+            ..
+        } => {
             let fullscreen = root.is_fullscreen();
             root.set_fullscreen(!fullscreen)
         }
@@ -108,8 +257,7 @@ fn handle_keys(root: &mut Root, player: &mut Object, map: &Map) -> bool {
         Key { code: Down, .. } => player.move_by(0, 1, map),
         Key { code: Left, .. } => player.move_by(-1, 0, map),
         Key { code: Right, .. } => player.move_by(1, 0, map),
-        _ => {},
-
+        _ => {}
     };
     false
 }
@@ -124,19 +272,17 @@ fn main() {
         .title("My Roguelike in Rust")
         .init();
 
-
     let mut con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
 
-    let player = Object::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, '@', colors::WHITE);
+    let mut player = Object::new(0, 0, '@', colors::WHITE);
+    let mut objects: Vec<Object> = vec![];
 
-    let npc = Object::new(SCREEN_WIDTH / 2 + 5, SCREEN_HEIGHT / 2 + 5, '@', colors::YELLOW);
+    let (map, (player_x, player_y)) = make_map(&mut objects);
 
-    let mut objects = [player, npc];
+    player.set_pos(player_x, player_y);
+    objects.insert(0, player);
 
-    let map = make_map();
-
-    while !root.window_closed(){
-
+    while !root.window_closed() {
         for object in &objects {
             object.draw(&mut con);
         }
@@ -152,8 +298,7 @@ fn main() {
         println!("{:?}", player);
         let exit = handle_keys(&mut root, player, &map);
         if exit {
-            break
+            break;
         }
     }
-
 }
